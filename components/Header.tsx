1f41ae650d9fc99bd4f8ee043b38c6c4da94e881
ozgurdenizozz <thecoderozz@gmail.com>
2026-06-02 18:00:31 +0300

    feat: Increase logo size in header for better visibility

--- components/Header.tsx
+++ components/Header.tsx
@@ -86,7 +86,7 @@ export default function Header() {
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16 lg:h-20">
+        <div className="flex items-center justify-between h-20 lg:h-24">
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2 flex-shrink-0">
             <Image
@@ -94,7 +94,7 @@ export default function Header() {
               alt="BY SUN Solar Energy Platform"
               width={240}
               height={80}
-              className="h-[52px] w-auto object-contain"
+              className="h-[64px] w-auto object-contain"
               priority
             />
           </Link>
